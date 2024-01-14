7983153a19de8b7a500e397a3a7f781d1d77466e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-14 18:26:42 +0200

    lab 3

--- 03. Basic CRUD/Lab/hotel_db.sql
+++ 03. Basic CRUD/Lab/hotel_db.sql
@@ -63,3 +63,9 @@ FROM `employees`
 WHERE `salary` > 1000
 ORDER BY `id`;
 
+-- 3 --
+UPDATE `employees`
+SET `salary` = `salary` + 100
+WHERE `job_title` = 'Manager';
+SELECT `salary` FROM `employees`;
+
