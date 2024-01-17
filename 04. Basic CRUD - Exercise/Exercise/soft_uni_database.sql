21f55997eabd9dd860fa1d7629130b575d9006bb
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-17 22:52:04 +0200

    ex 19-20

--- 04. Basic CRUD - Exercise/Exercise/soft_uni_database.sql
+++ 04. Basic CRUD - Exercise/Exercise/soft_uni_database.sql
@@ -1792,3 +1792,14 @@ FROM `projects`
 ORDER BY `start_date`, `name` 
 LIMIT 10;
 
+-- 19 --
+SELECT `first_name`, `last_name`, `hire_date`
+FROM `employees`
+ORDER BY `hire_date` DESC
+LIMIT 7;
+
+-- 20 --
+UPDATE `employees`
+SET `salary` = `salary` * 1.12
+WHERE `department_id` IN (1, 2, 4, 11);
+SELECT `salary` FROM `employees`;
