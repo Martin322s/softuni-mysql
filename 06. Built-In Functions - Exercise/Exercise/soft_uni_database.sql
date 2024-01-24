e364b596b342330d1be18b102e34230cffa6c07e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-24 20:57:59 +0200

    ex 1

--- 06. Built-In Functions - Exercise/Exercise/soft_uni_database.sql
+++ 06. Built-In Functions - Exercise/Exercise/soft_uni_database.sql
@@ -1673,3 +1673,30 @@ INSERT INTO `towns` (`town_id`, `name`) VALUES
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 
+-- 1 --
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE LOWER(SUBSTRING(`first_name`, 1, 2)) = 'sa'
+ORDER BY `employee_id`;
+SELECT `town_id`, `name`
+FROM `towns`
+WHERE SUBSTRING(LOWER(`name`), 1, 1) IN ('m', 'k', 'e', 'b')
+ORDER BY `name`;
+
+-- 7 --
+SELECT `town_id`, `name`
+FROM `towns`
+WHERE LEFT(LOWER(`name`), 1) NOT IN ('r', 'b', 'd')
+ORDER BY `name`;
+
+-- 8 --
+CREATE VIEW `v_employees_hired_after_2000` AS
+	SELECT `first_name`, `last_name`
+    FROM `employees`
+    WHERE YEAR(`hire_date`) > 2000;
+SELECT * FROM `v_employees_hired_after_2000`;
+
+-- 9 --
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE LENGTH(`last_name`) = 5;
