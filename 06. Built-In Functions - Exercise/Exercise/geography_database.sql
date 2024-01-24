5ffe2a28f37047c2e3db2531de34b5412847af83
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-24 20:56:37 +0200

    ex 11

--- 06. Built-In Functions - Exercise/Exercise/geography_database.sql
+++ 06. Built-In Functions - Exercise/Exercise/geography_database.sql
@@ -807,3 +807,8 @@ FROM `countries`
 WHERE LOWER(`country_name`) LIKE '%a%a%a%'
 ORDER BY `iso_code`;
 
+-- 11 --
+SELECT `peak_name`, `river_name`, CONCAT(LOWER(`peak_name`), SUBSTRING(LOWER(`river_name`), 2)) AS 'mix' 
+FROM `peaks`, `rivers`
+WHERE RIGHT(`peak_name`, 1) = LEFT(`river_name`, 1)
+ORDER BY `mix`;
