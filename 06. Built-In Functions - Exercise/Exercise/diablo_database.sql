202c8aec05806e5be9842642139271eecc861503
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-24 20:55:52 +0200

    ex 15

--- 06. Built-In Functions - Exercise/Exercise/diablo_database.sql
+++ 06. Built-In Functions - Exercise/Exercise/diablo_database.sql
@@ -4125,3 +4125,17 @@ FROM `users`
 WHERE `ip_address` LIKE '___.1%.%.___'
 ORDER BY `user_name`;
 
+-- 15 --
+SELECT `name` AS `game`,
+CASE 
+	WHEN HOUR(`start`) < 12 THEN 'Morning'
+	WHEN HOUR(`start`) < 18 THEN 'Afternoon'
+	WHEN HOUR(`start`) < 24 THEN 'Evening'
+END AS 'Part of the Day',
+CASE
+	WHEN `duration` < 4 THEN 'Extra Short'
+	WHEN `duration` < 7 THEN 'Short'
+	WHEN `duration` < 11 THEN 'Long'
+	ELSE 'Extra Long'
+END AS 'Duration'
+FROM `games`;
