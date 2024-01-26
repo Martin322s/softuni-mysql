24bee3d40c3dd0e3e700d0785acd3313207d13d1
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-26 13:10:44 +0200

    ex-3

--- 12. Subqueries and JOINs - Exercise/Exercise/soft_uni_database.sql
+++ 12. Subqueries and JOINs - Exercise/Exercise/soft_uni_database.sql
@@ -14,4 +14,12 @@ JOIN `towns` AS `t` ON `t`.`town_id` = `a`.`town_id`
 ORDER BY `first_name`, `last_name`
 LIMIT 5;
 
+-- 3 --
+SELECT `employee_id`, `first_name`, `last_name`, `name`
+FROM `employees` AS `e`
+JOIN `departments` AS `d`
+ON `e`.`department_id` = `d`.`department_id`
+WHERE `name` = 'Sales'
+ORDER BY `employee_id` DESC;
 
+-- 4 --
