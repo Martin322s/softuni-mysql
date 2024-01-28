b303256155b0d2ec2654466940fe8a27b4c61735
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-28 12:47:33 +0200

    lab 5

--- 07. Data Aggregation/Lab/restaurant.sql
+++ 07. Data Aggregation/Lab/restaurant.sql
@@ -21,3 +21,11 @@ FROM `products`
 WHERE `category_id` = 2 AND `price` > 8
 GROUP BY `category_id`;
 
+ -- 5 --
+SELECT 
+	`category_id`, 
+    ROUND(AVG(`price`), 2) AS `Average Price`, 
+    ROUND(MIN(`price`), 2) AS `Cheapest Product`,
+    ROUND(MAX(`price`), 2) AS `Most Expensive Product`
+FROM `products`
+GROUP BY `category_id`;
