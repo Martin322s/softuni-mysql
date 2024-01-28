6f95fe0c6a2987b5bfcd0893cf5309018f7a6e9f
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-28 12:47:00 +0200

    lab 2

--- 07. Data Aggregation/Lab/restaurant.sql
+++ 07. Data Aggregation/Lab/restaurant.sql
@@ -3,3 +3,9 @@ SELECT `department_id`, COUNT(`id`) AS `Number of employees`
 FROM `employees`
 GROUP BY `department_id`;
 
+-- 2 --
+SELECT `department_id`, ROUND(AVG(`salary`), 2) 
+FROM `employees` 
+GROUP BY `department_id` 
+ORDER BY `department_id`;
+
