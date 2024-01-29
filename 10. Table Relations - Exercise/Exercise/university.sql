fedea2b1a814dbf2324f82d21e79b99a0116c596
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-29 12:43:44 +0200

    university-tables

--- 10. Table Relations - Exercise/Exercise/university.sql
+++ 10. Table Relations - Exercise/Exercise/university.sql
@@ -21,25 +21,3 @@ CREATE TABLE `students` (
     REFERENCES `majors`(`major_id`)
 );
 
-CREATE TABLE `payments` (
-	`payment_id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `payment_date` DATE,
-    `payment_amount` DECIMAL(8, 2),
-    `student_id` INT,
-    CONSTRAINT `fk_payments_students_id`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `students`(`student_id`)
-);
-
-CREATE TABLE `agenda` (
-	`student_id` INT,
-    `subject_id` INT,
-    CONSTRAINT `pk_students_subjects_id`
-    PRIMARY KEY (`student_id`, `subject_id`),
-    CONSTRAINT `fk_students_id`
-    FOREIGN KEY (`student_id`)
-    REFERENCES `students`(`student_id`),
-    CONSTRAINT `fk_subjects_id`
-    FOREIGN KEY (`subject_id`)
-    REFERENCES `subjects`(`subject_id`)
-);
