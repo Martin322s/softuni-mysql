fc793f44abfd955087b220537b2d571743a2a0c9
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-31 14:47:16 +0200

    gringotts 0 6

--- 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
+++ 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
@@ -25,3 +25,10 @@ FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `total_sum`;
 
+-- 6 --
+SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+ORDER BY `deposit_group`;
+
