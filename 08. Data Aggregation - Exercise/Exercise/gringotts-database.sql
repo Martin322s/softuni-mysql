3f5182903b855c4273642fcbf6b4ab3844ae5ad8
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-31 14:46:40 +0200

    gringotts 3, 4

--- 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
+++ 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
@@ -6,3 +6,16 @@ FROM `wizzard_deposits`;
 SELECT MAX(`magic_wand_size`) AS `longest_magic_wand`
 FROM `wizzard_deposits`;
 
+-- 3 --
+SELECT `deposit_group`, MAX(`magic_wand_size`) AS `longest_magic_wand`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `longest_magic_wand`, `deposit_group`;
+
+-- 4 --
+SELECT `deposit_group`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+HAVING MIN(`magic_wand_size`)
+LIMIT 1;
+
