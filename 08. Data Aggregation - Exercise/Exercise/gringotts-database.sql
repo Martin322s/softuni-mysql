618598f550babb39d34a033ec61aa1bcb1eb221b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-31 14:46:58 +0200

    gringotts - 5

--- 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
+++ 08. Data Aggregation - Exercise/Exercise/gringotts-database.sql
@@ -19,3 +19,9 @@ GROUP BY `deposit_group`
 HAVING MIN(`magic_wand_size`)
 LIMIT 1;
 
+-- 5 --
+SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum`;
+
