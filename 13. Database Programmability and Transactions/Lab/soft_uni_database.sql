be77a50b721c6970845a022736ea79a878e99a7b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-03 23:54:23 +0200

    lab - 4

--- 13. Database Programmability and Transactions/Lab/soft_uni_database.sql
+++ 13. Database Programmability and Transactions/Lab/soft_uni_database.sql
@@ -62,3 +62,43 @@ CALL usp_raise_salary_by_id(17);
 
 SELECT `salary` FROM `employees` WHERE `employee_id` = 17;
 
+-- 4 --
+CREATE TABLE deleted_employees (
+	employee_id INT PRIMARY KEY AUTO_INCREMENT, 
+	first_name VARCHAR(20),
+	last_name VARCHAR(20),
+	middle_name VARCHAR(20),
+	job_title VARCHAR(50),
+	department_id INT,
+	salary DECIMAL(19,4)
+);
+
+DELIMITER $
+
+CREATE TRIGGER tr_delete_employees
+AFTER DELETE
+ON `employees`
+FOR EACH ROW
+BEGIN
+	INSERT INTO deleted_employees (
+		first_name,
+		last_name,
+		middle_name,
+		job_title,
+		department_id,
+		salary)
+	VALUES(
+		OLD.first_name,
+		OLD.last_name,
+		OLD.middle_name,
+		OLD.job_title,
+		OLD.department_id,
+		OLD.salary);
+END $
+
+DELIMITER ;
+
+DELETE FROM employees WHERE employee_id = 2;
+
+SELECT *
+FROM `deleted_employees`;
