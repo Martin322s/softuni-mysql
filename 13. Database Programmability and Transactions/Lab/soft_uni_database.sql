c3552d579083a90eab5e409c4e6315cd47424381
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-03 23:53:58 +0200

    lab - 2

--- 13. Database Programmability and Transactions/Lab/soft_uni_database.sql
+++ 13. Database Programmability and Transactions/Lab/soft_uni_database.sql
@@ -22,3 +22,25 @@ DELIMITER ;
 
 SELECT ufn_count_employees_by_town('Sofia');
 
+-- 2 --
+DELIMITER $
+
+CREATE PROCEDURE usp_raise_salaries(department_name VARCHAR(50))
+BEGIN
+	UPDATE `employees` as `e`
+    JOIN `departments` AS `d` 
+    ON `e`.`department_id` = `d`.`department_id`
+    SET `salary` = `salary` * 1.05
+    WHERE department_name = `d`.`name`;
+END $
+
+DELIMITER ;
+
+CALL usp_raise_salaries('Finance');
+SELECT `first_name`, `salary` 
+FROM `employees` AS `e`
+JOIN `departments` AS `d`
+ON `e`.`department_id` = `d`.`department_id`
+WHERE `d`.`name` = 'Finance' 
+ORDER BY `first_name`, `salary`;
+
