304f398535451f7b29e8bb764c58aacb1f639a64
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-06 12:07:53 +0200

    ex-4

--- 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
+++ 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
@@ -43,3 +43,22 @@ DELIMITER ;
 
 CALL usp_get_towns_starting_with('b');
 
+-- 4 --
+DELIMITER $
+
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(50))
+BEGIN
+	SELECT `first_name`, `last_name`
+    FROM `employees` AS `e`
+    JOIN `addresses` AS `a`
+    ON `e`.`address_id` = `a`.`address_id`
+    JOIN `towns` AS `t`
+    ON `a`.`town_id` = `t`.`town_id`
+    WHERE `t`.`name` = town_name
+    ORDER BY `first_name`, `last_name`, `employee_id`;
+END $
+
+DELIMITER ;
+
+CALL usp_get_employees_from_town('Sofia');
+
