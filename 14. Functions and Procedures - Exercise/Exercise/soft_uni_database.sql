efe672d5ef91597f5c13ee5591182453071e900d
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-06 12:06:22 +0200

    ex-2

--- 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
+++ 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
@@ -12,3 +12,19 @@ END $
 DELIMITER ;
 
 CALL usp_get_employees_salary_above_35000();
+
+-- 2 --
+DELIMITER $
+
+CREATE PROCEDURE usp_get_employees_salary_above(given_salary DECIMAL(10, 4))
+BEGIN
+	SELECT `first_name`, `last_name`
+    FROM `employees`
+    WHERE `salary` >= given_salary
+    ORDER BY `first_name`, `last_name`, `employee_id`;
+END $
+
+DELIMITER ;
+
+CALL usp_get_employees_salary_above(45000);
+
