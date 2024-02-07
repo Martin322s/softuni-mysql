f093bcb3e18c014a868933892004f5247d659223
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-07 12:10:07 +0200

    ex-8

--- 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
+++ 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
@@ -120,3 +120,15 @@ END $
 
 DELIMITER ;
 
+-- 8 --
+DELIMITER $
+
+CREATE PROCEDURE usp_get_holders_full_name()
+BEGIN
+	SELECT CONCAT_WS(' ', `first_name`, `last_name`) AS `full_name`
+	FROM `account_holders`
+	ORDER BY `full_name`, `id`;
+END $
+
+DELIMITER ;
+
