bf32a4bc58dd37d437b645eb008c127fc6849c48
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-07 12:09:13 +0200

    ex-6

--- 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
+++ 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
@@ -84,3 +84,16 @@ DELIMITER ;
 SELECT `salary`, ufn_get_salary_level(`salary`) AS `salary_level`
 FROM `employees`;
 
+-- 6 --
+DELIMITER $
+
+CREATE PROCEDURE `usp_get_employees_by_salary_level` (salary_level VARCHAR(7))
+BEGIN
+SELECT `first_name`, `last_name`
+FROM `employees`
+WHERE ufn_get_salary_level(`salary`) = salary_level
+ORDER BY `first_name` DESC, `last_name` DESC;
+END $
+
+DELIMITER ;
+
