88a326a4dde7bb5c271d16f72f629aaf2bb09b52
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-07 12:12:28 +0200

    ex-14

--- 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
+++ 14. Functions and Procedures - Exercise/Exercise/soft_uni_database.sql
@@ -219,3 +219,29 @@ END $
 
 DELIMITER ;
 
+-- 14 --
+DELIMITER $
+
+CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(19, 4))
+BEGIN
+	START TRANSACTION;
+	IF((SELECT count(`a`.`id`) FROM `accounts` AS `a` WHERE `a`.`id` = to_account_id) != 1 
+    OR (SELECT count(`a`.`id`) FROM `accounts` AS `a` WHERE `a`.`id` = from_account_id) != 1 
+    OR amount < 0 
+    OR (SELECT `balance` FROM `accounts` AS `a` WHERE `a`.`id` = from_account_id) < amount
+    OR from_account_id = to_account_id
+    ) 
+    THEN
+	ROLLBACK;
+	ELSE
+		UPDATE `accounts`
+        SET `balance` = `balance` - amount
+		WHERE `accounts`.`id` = from_account_id;
+        UPDATE `accounts` 
+        SET `balance` = `balance` + amount
+		WHERE `accounts`.`id` = to_account_id;
+	END IF; 
+END $
+
+DELIMITER ;
+
