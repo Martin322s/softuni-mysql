5228911a8ce336e46dcca05a65a1337edc3eb6f4
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-12 12:25:43 +0200

    ex-8

--- 16. Regular Exam/exam.sql
+++ 16. Regular Exam/exam.sql
@@ -310,3 +310,13 @@ WHERE `is_armed` = 1
 GROUP BY `name`
 ORDER BY `armed_workers` DESC, `name`;
 
+SELECT `p`.`name`, `c`.`country_code`, YEAR(`established_on`) AS `founded_in`
+FROM `preserves` AS `p`
+JOIN `countries_preserves` AS `cp`
+ON `p`.`id` = `cp`.`preserve_id`
+JOIN `countries` AS `c`
+ON `cp`.`country_id` = `c`.`id`
+WHERE MONTH(`established_on`) = 5
+ORDER BY `established_on` ASC
+LIMIT 5;
+
