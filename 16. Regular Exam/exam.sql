bff9d87f03dd6adb4c574c3e9bb3e6f29bf01a88
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-02-12 12:26:02 +0200

    ex-9

--- 16. Regular Exam/exam.sql
+++ 16. Regular Exam/exam.sql
@@ -320,3 +320,16 @@ WHERE MONTH(`established_on`) = 5
 ORDER BY `established_on` ASC
 LIMIT 5;
 
+SELECT 
+	`id`, 
+    `name`,
+    CASE 
+		WHEN `area` <= 100 THEN 'very small'
+        WHEN `area` >= 100 AND `area` <= 1000 THEN 'small'
+        WHEN `area` > 1000 AND `area` <= 10000 THEN 'medium'
+        WHEN `area` > 10000 AND `area` <= 50000 THEN 'large'
+        WHEN `area` > 50000 THEN 'very large'
+    END AS `category`
+FROM `preserves`
+ORDER BY `area` DESC;
+
